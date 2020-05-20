5315e4f5f725a315bc304381b44c0af88dc412f8
dgargantilla <dgargantilla@webimpacto.es>
2020-05-20 14:22:24 +0200

    Empezamos con la maquetacion en nuxt.js y intentamos añadir las fonts

---
+++ proyectonux/layouts/Registro.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #fa2398;
+  background-image: linear-gradient(180deg, #fa2398 0%, #dd1f94 100%) no-repeat
+    center center;
+  background-size: auto;
+}
+</style>
---
+++ proyectonux/pages/Registro/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container-fluid p-0 cf">
+    <div class="row justify-content-center p-0 m-0 fila">
+      <div class="contenedor">
+        <div class="col">
+            
+            <div class="row justify-content-around fila3 mt-2 mt-sm-3">
+                <div class="col-4 col-xs-5 col-sm-5 pl-2 pl-sm-3 c1">
+                    <img id="img1" src="~/assets/img/go-back.svg" />
+                    <span class="span1">
+                        Volver
+                    </span>
+                </div>
+                <div class="col-8 col-xs-7 col-sm-7 pr-2 c">
+                    <span class="span2">
+                        Ya tengo cuenta
+                    </span>
+                    <img id="img2" src="~/assets/img/User.svg" />
+                </div> 
+            </div>
+            <div class="row filalogo justify-content-center mt-5">
+                <img src="~/assets/img/Nutritienda-simple_logo.svg">
+            </div>
+
+            <div class="row justify-content-around mt-5 filaLoguin">
+              <div class="col-8 col-sm-7 new">
+                <h4 class="span3">Crear nueva cuenta</h4>
+              </div>
+            </div>
+
+            <div class="row justify-content-center mt-1 filaA">
+                <div class="col-12">
+                    <span class="span4">Únete y disfruta de todas las ventajas: pedidos rápidos, descuento, promociones, beneficios y muchos más!</span>
+                </div>
+            </div>
+            <div class="row justify-content-center filaicone">
+                <div class="col-3">
+                    <img src="~/assets/img/Delivery.svg">
+                    <span>Envío gratuito</span>
+                </div>
+                <div class="col-3">
+                    <img src="~/assets/img/best_price.svg">
+                    <span>Mejor Precio</span>
+                </div>
+                <div class="col-3">
+                    <img src="~/assets/img/lock.svg">
+                    <span>Pago 100% seguro</span>
+                </div>
+                <div class="col-3">
+                    <img src="~/assets/img/Superprice.svg">
+                    <span>Ofertas exclusivas</span>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //   layout: "Registro"
+  head:{
+      title: 'Registrate',
+  }
+};
+</script>
+
+<style src="~/assets/css/style.css" scoped>
+
